d694c40d4545642cdd8bb6ca1c0591423ff9507b
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-08-09 20:29:05 +0700

    Use CSS instead of JS for password error messages

--- script.js
+++ script.js
@@ -3,14 +3,6 @@ const passwordContainer = document.querySelector('.password-inputs p:nth-child(1
 const confirmPassword = document.querySelector('#confirmpass');
 const confirmPasswordContainer = document.querySelector('.password-inputs p:nth-child(2)');
 
-const passwordError = document.createElement('p');
-passwordContainer.appendChild(passwordError);
-
 passwordInput.addEventListener('input', () => {
     confirmPassword.setAttribute("pattern", `${passwordInput.value}`);
-    if (passwordInput.value.length < 8 && passwordInput.value.length > 0) {
-        passwordError.textContent = "Password needs to be at least 8 characters";
-    } else {
-        passwordError.textContent = "";
-    };
 });
