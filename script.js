09abdb311f697d4ad51a37ff46f83f0eabfecfca
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-08-09 19:22:45 +0700

    Add error message when pass length is less than 8 characters

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const passwordInput = document.querySelector('#password');
+const passwordContainer = document.querySelector('.password-inputs p:nth-child(1)');
 const confirmPassword = document.querySelector('#confirmpass');
+const confirmPasswordContainer = document.querySelector('.password-inputs p:nth-child(2)');
 
-passwordInput.addEventListener('change', () => {
+const passwordError = document.createElement('p');
+passwordContainer.appendChild(passwordError);
+
+passwordInput.addEventListener('input', () => {
     confirmPassword.setAttribute("pattern", `${passwordInput.value}`);
+    if (passwordInput.value.length < 8 && passwordInput.value.length > 0) {
+        passwordError.textContent = "Password needs to be at least 8 characters";
+    } else {
+        passwordError.textContent = "";
+    };
 });
