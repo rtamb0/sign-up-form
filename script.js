5e2e5716d1a78247edbb423eeff3718d237233f0
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-08-09 17:23:51 +0700

    Add password validation

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const passwordInput = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirmpass');
+
+passwordInput.addEventListener('change', () => {
+    confirmPassword.setAttribute("pattern", `${passwordInput.value}`);
+});
