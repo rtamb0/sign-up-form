c2f37039504442d1bbc30905cb221b0566c9ab3d
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-08-11 16:43:13 +0700

    Modify the password input validation messages based on the situation

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const passwordInput = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirmpass');
+const confirmPasswordError = document.querySelector('input[id="confirmpass"] + span');
 
 passwordInput.addEventListener('input', () => {
     confirmPassword.setAttribute("pattern", `${passwordInput.value}`);
-    if (passwordInput.value.length >= 8) {
-        confirmPassword.disabled = false;
+    if (passwordInput.value.length < 8) {
+        confirmPassword.setCustomValidity("Please fill up the password field first.");
+        confirmPasswordError.classList.add('fill-up-message');
     }
     else {
-        confirmPassword.disabled = true;
-        if (passwordInput.value.length <= 1) {
-            confirmPassword.value = '';
-        };
+        confirmPassword.setCustomValidity("");
+        confirmPasswordError.classList.remove('fill-up-message');
     };
 });
